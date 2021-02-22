a9242b3b6b74a8185aa14d0c1d7c7c11b3890fc5
CROCKOLACO <crockolaco@yandex.ru>
2021-02-22 16:11:12 +0500

    feat(features/apod): Create

---
+++ src/features/apod/apod.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const Apod = () => {
+  return <div>Apod</div>;
+};
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
 
+import { Apod } from "features/apod";
+
 export const HomePage = () => {
-  return <span>Привет. Тест шрифта JetBrainsMonoRegular.</span>;
+  return (
+    <>
+      <span>Привет. Тест шрифта JetBrainsMonoRegular.</span>
+      <Apod />
+    </>
+  );
 };
