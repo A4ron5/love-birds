f71331637ac85d19cff1667eb138648f8ee2d76b
CROCKOLACO <crockolaco@yandex.ru>
2021-02-25 17:20:54 +0500

    fix(page/home): add template

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 
 import { Apod } from "features/apod";
+import { MainTemplate } from "ui/templates";
 
 export const HomePage = () => {
   return (
-    <>
+    <MainTemplate>
       <span>Привет. Тест шрифта JetBrainsMonoRegular.</span>
       <Apod />
-    </>
+    </MainTemplate>
   );
 };
