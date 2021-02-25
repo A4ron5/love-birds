ee3b5966a3b81266dba4e8a0e803c541bb7154d9
CROCKOLACO <crockolaco@yandex.ru>
2021-02-25 17:19:08 +0500

    feat(ui/molecules/burger): create

---
+++ src/ui/molecules/burger/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const Menu = styled.div`
+  width: 25px;
+  height: 25px;
+  background: aqua;
+`;
+
+export const Burger = () => <Menu />;
