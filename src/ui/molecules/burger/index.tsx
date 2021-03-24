f29bcdc57de945b83a707ca16839f4c03ad4546b
Maksim Safin <crockolaco@yandex.ru>
2021-03-24 16:39:01 +0500

    feat(ui/molecules/burger): create

--- src/ui/molecules/burger/index.tsx
+++ src/ui/molecules/burger/index.tsx
@@ -1,10 +1,58 @@
 import * as React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const Menu = styled.div`
+const line = css`
   width: 25px;
-  height: 25px;
-  background: aqua;
+  height: 4px;
+  background: var(--primary-color);
+  transition: all 0.2s ease-in-out;
 `;
 
-export const Burger = () => <Menu />;
+const Menu = styled.div`
+  span {
+    position: relative;
+    display: block;
+    ${line}
+
+    &:before,
+    &:after {
+      content: "";
+      position: absolute;
+      ${line}
+    }
+    &:before {
+      top: -8px;
+    }
+
+    &:after {
+      top: 8px;
+    }
+  }
+
+  &[data-open="true"] {
+    span {
+      background: transparent;
+
+      &:before {
+        transform: rotate(45deg) translate(5px, 6px);
+      }
+
+      &:after {
+        transform: rotate(-45deg) translate(5px, -6px);
+      }
+    }
+  }
+`;
+
+type Props = {
+  open: boolean;
+  clickHandler: () => void;
+};
+
+export const Burger = ({ open, clickHandler }: Props) => {
+  return (
+    <Menu data-open={open} onClick={clickHandler}>
+      <span />
+    </Menu>
+  );
+};
