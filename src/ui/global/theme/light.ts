cedeb33147f8bef493410663401ae8a052404ea8
Maksim Safin <crockolaco@yandex.ru>
2021-03-26 08:43:26 +0500

    fix(ui/global/theme): change secondary color

--- src/ui/global/theme/light.ts
+++ src/ui/global/theme/light.ts
@@ -2,7 +2,7 @@ import { css } from "styled-components";
 
 export const light = css`
   --primary-color: hsl(10, 20%, 20%);
-  --secondary-color: hsl(10, 80%, 50%);
+  --secondary-color: hsl(5, 40%, 60%);
   --tertiary-color: hsl(9, 20%, 20%);
   --background-color: hsl(60, 8%, 95%);
   --border-color: hsl(22, 22%, 90%);
