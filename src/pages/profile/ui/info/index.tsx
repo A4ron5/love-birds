e4532e198c2dd6e191ff5e3658086e46d00cd089
Maksim Safin <crockolaco@yandex.ru>
2021-06-14 19:00:54 +0500

    feat(pages/profile/ui/info): create

---
+++ src/pages/profile/ui/info/index.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import styled from "styled-components";
+import firebase from "firebase/app";
+
+const FieldContainer = styled.div``;
+const FieldName = styled.p`
+  font-weight: bold;
+`;
+const FieldData = styled.span``;
+
+const Field = ({ name, data }: { name: string; data: string | null }) => {
+  if (!data) return null;
+
+  return (
+    <FieldContainer>
+      <FieldName>{name}:</FieldName>
+      <FieldData>{data}</FieldData>
+    </FieldContainer>
+  );
+};
+
+export const Info = ({ user }: { user: firebase.User | null }) => {
+  if (!user) return null;
+
+  const { email, emailVerified, displayName } = user;
+
+  const isEmailVerified = emailVerified ? "yes" : "no";
+
+  return (
+    <section>
+      <Field name="Email" data={email} />
+      <Field name="Email verified" data={isEmailVerified} />
+      <Field name="Display name" data={displayName} />
+    </section>
+  );
+};
