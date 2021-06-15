ad3bed1091d9b6c7d3c2d07cc4b1717773783873
Maksim Safin <crockolaco@yandex.ru>
2021-06-15 21:26:35 +0500

    feat(pages/profile/model): implement logout

--- src/pages/profile/model/profile.init.ts
+++ src/pages/profile/model/profile.init.ts
@@ -1,13 +1,27 @@
-import { guard } from "effector";
+import { guard, forward } from "effector";
 
 import { historyPush } from "features/common/routing";
 import { $user } from "features/user/model";
 
-import { ProfilePageGate } from "./profile.model";
+import { logout as logoutHandler } from "../lib/logout";
+
+import { ProfilePageGate, logout, fxLogout } from "./profile.model";
+
+fxLogout.use(logoutHandler);
 
 guard({
   source: $user,
   clock: ProfilePageGate.open,
   filter: (user) => !user?.email,
   target: historyPush.prepend(() => "/auth")
+});
+
+forward({
+  from: logout,
+  to: fxLogout
+});
+
+forward({
+  from: fxLogout.done,
+  to: historyPush.prepend(() => "/auth")
 });
--- src/pages/profile/model/profile.model.ts
+++ src/pages/profile/model/profile.model.ts
@@ -1,3 +1,9 @@
 import { createGate } from "effector-react";
 
+import { root } from "lib/effector-root";
+
+export const logout = root.createEvent();
+
+export const fxLogout = root.createEffect<void, void>();
+
 export const ProfilePageGate = createGate();
