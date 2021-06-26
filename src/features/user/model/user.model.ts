391c3c0e15fb1d8ba79af7268ca62bce3f5c707c
Maksim Safin <crockolaco@yandex.ru>
2021-06-26 17:08:51 +0500

    feat(features/user/model): migrate from firebase 8 to 9.beta

--- src/features/user/model/user.model.ts
+++ src/features/user/model/user.model.ts
@@ -1,16 +1,13 @@
-import firebase from "firebase";
+import { User } from "firebase/auth";
 import { createGate } from "effector-react";
 
 import { root } from "lib/effector-root";
 
 export const clickUserIcon = root.createEvent<void>();
 
-export const fxOnAuthStateChanged = root.createEffect<
-  undefined,
-  firebase.User | null
->();
+export const fxOnAuthStateChanged = root.createEffect<undefined, User | null>();
 
-export const $user = root.createStore<firebase.User | null>(null);
+export const $user = root.createStore<User | null>(null);
 export const $loading = root.createStore<boolean>(false);
 
 export const UserGate = createGate();
