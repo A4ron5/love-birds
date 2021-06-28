d6201083cba11a6f529307885e2995fc62f9689c
Maksim Safin <crockolaco@yandex.ru>
2021-06-28 18:35:17 +0500

    chore(webpack.config): temporary change hash

--- webpack.config.ts
+++ webpack.config.ts
@@ -32,7 +32,7 @@ module.exports = () => {
     },
     output: {
       path: path.join(__dirname, "/dist"),
-      filename: "[name].bundle.[contenthash].min.js"
+      filename: "[name].bundle.[hash].min.js"
     },
     devServer: {
       historyApiFallback: true
